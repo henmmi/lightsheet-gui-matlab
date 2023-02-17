aeba2c6a7aa0571c11230253f51fe8c7478646e1
Henry Nguyen <henmmi@outlook.com>
2023-02-17 12:30:48 +1030

    Updated GUI, implemented move settings & acquire settings

--- run_acquire.m
+++ run_acquire.m
@@ -1,7 +1,10 @@
 
-function run_acquire(serial_device, distance, num_images)
+function run_acquire(serial_device, start_position, end_position, num_images)
 
+distance = start_position - end_position;
 d = distance/num_images;
+wait_ready(serial_device);
+absolute_move(start_position, serial_device);
 wait_ready(serial_device);
 
 for i = 1:num_images
