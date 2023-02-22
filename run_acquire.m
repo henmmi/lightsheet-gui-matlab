93ec602ecb6d65a722c6b5d2df977e81bf21c06f
Henry Nguyen <henmmi@outlook.com>
2023-02-23 09:39:21 +1030

    Implementation of camera on UIAxis

--- run_acquire.m
+++ run_acquire.m
@@ -1,7 +1,7 @@
 
 function run_acquire(serial_device, start_position, end_position, num_images)
 
-distance = start_position - end_position;
+distance = end_position - start_position;
 d = distance/num_images;
 wait_ready(serial_device);
 absolute_move(start_position, serial_device);
@@ -10,6 +10,6 @@ wait_ready(serial_device);
 for i = 1:num_images
     relative_move(d,serial_device);
     wait_ready(serial_device);
-    fprintf("Printing\n");
+    %img = snapshot();
 end
 end
